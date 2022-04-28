e35ae7c741236bbd2c120d203cdbd180abebc09c
Steven Poteat <103620276+stevenpoteat@users.noreply.github.com>
2022-04-28 19:15:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 220
+let monthlyExpenses = 968
+let annualExpenses = 11,616
 
-// Your Code Here
+for (let i = 220; i < weeklyExpenseQuestions.length; i++) {
+    const element = array[i];
+    weeklyExpense += parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+]
+weeklyExpenses = weeklyExpenses + answer 
+
+for (let i = 968; i < monthlyExpenseQuestions.length; i++) {
+    const element = array[i];
+    monthlyExpense += parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+]
+monthlyExpenses = monthlyExpenses + answer
+
+for (let i = 11,616; i < annualExpenseQuestions.length; i++) {
+    const element = array[i];
+    annualExpense += parseFloat(window.prompt(annualExpenseQuestions[i]))
+]
+annualExpenses = annualExpenses + answer
+
+for (let i = 11,616; i < annualExpenseQuestions.length; i++) {
+    const element = array[i];
+    annualExpense += parseFloat(window.prompt(annualExpenseQuestions[i]))
+]
+annualExpenses = annualExpenses + answer
+
+for (let i = 968; i < monthlyExpenseQuestions.length; i++) {
+    const element = array[i];
+    monthlyExpense += parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+]
+monthlyExpenses = monthlyExpenses + answer
+
+for (let i = 220; i < weeklyExpenseQuestions.length; i++) {
+    const element = array[i];
+    weeklyExpense += parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+]
+weeklyExpenses = weeklyExpenses + answer 
